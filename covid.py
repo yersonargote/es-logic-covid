1e25397d507a1618ef3347403d5f132843316694
Lexson <llalex.2045@gmail.com>
2022-01-20 21:29:28 -0500

    Upgrade: Sintomas por consola

--- covid.py
+++ covid.py
@@ -7,10 +7,8 @@ def check_knowledge(knowledge, symbols):
     for symbol in symbols:
         if model_check(knowledge, symbol):
             print(f"    {symbol}")
-   
-    return knowledge, diagnostics
-
     
+
 test_symptomatic = And(
     test,
     Not(dyspnea),
@@ -45,8 +43,7 @@ test_healthy = And(
 )
 
 
-def main():
-
+def manual_test():
     symp: And  = deepcopy(knowledge)
     asymp: And = deepcopy(knowledge)
     res_dis: And = deepcopy(knowledge)
@@ -67,6 +64,59 @@ def main():
     for test, know in tests:
         print(test)
         check_knowledge(knowledge=know, symbols=diagnostics)
+
+
+def yes_no():
+    exit = False
+    yes = False
+    while not exit:
+        inpt = input("si/no. 'sal' para salir. ")
+        if inpt == 'si':
+            yes = True
+        exit = (inpt == 'si' or inpt == 'no' or inpt == 'sal')
+    return yes
+
+
+def main():
+    valid = False
+    info = And()
+
+    print('Diagnostico del covid-19')
+    while not valid:
+        print('Indique estado de la prueba de covid-19.')
+        print('1. Positiva.')
+        print('2. Negativa.')
+        inpt = input()
+        if inpt == '1':
+            info.add(test)
+        elif inpt == '2':
+            info.add(Not(test))
+        valid = (inpt == '1' or inpt == '2')
+
+    print('Presenta fiebre.')
+    yes = yes_no()
+    if yes:
+        info.add(fever)
+    else:
+        info.add(Not(fever))
+
+    print('Presenta tos.')
+    yes = yes_no()
+    if yes:
+        info.add(cough)
+    else:
+        info.add(Not(cough))
+
+    print('Presenta disnea.')
+    yes = yes_no()
+    if yes:
+        info.add(dyspnea)
+    else:
+        info.add(Not(dyspnea))
+
+    knowledge.add(info)
+    print('Diagnostico: ', end='')
+    check_knowledge(knowledge=knowledge, symbols=diagnostics)
 
 
 if __name__ == '__main__':
